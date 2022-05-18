711aae94c2ce580d6a593f6bbad2930124a429bc
ivanalexisc <iac_1999@hotmail.com>
2022-05-18 01:29:09 -0300

    agregue el detalle del producto con el id y multer

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const productsFileJson = path.join(__dirname, '../data/productsDB.json');
 const products = JSON.parse(fs.readFileSync(productsFileJson, 'utf-8'));
+let guardar = (products) => {
+	fs.writeFileSync(
+	  path.join(__dirname, "../data/productsDataBase.json"),
+	  JSON.stringify(products, null, " "),
+	  "utf-8"
+	);
+  };
 
 const controller = {
     showProduct: (req, res) => {
@@ -15,7 +22,20 @@ const controller = {
         }
     },
     create: (req,res) => {
-        res.render('product-create-form')
+        res.render('product-create-form');
+    },
+    store: (req,res) => {
+
+        let newProduct = {
+            id:products[products.length -1].id + 1,
+            ...req.body,
+           image: req.file.filename
+        }
+        products.push(newProduct);
+        guardar(products);
+        res.redirect('/products');
+
+
     }
 };
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
+
+//Controller require
 const productController = require('../controllers/productController');
+
+//SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/images'))
+    },
+    filename: function (req, file, cb) {
+        console.log(file);
+      cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+    }
+  });
+   
+  var upload = multer({ 
+      storage,
+
+      //VALIDATE IMAGE
+      fileFilter: (req,file,cb) => {
+          const acceptedExtensions = ['.jpg', '.jpeg', '.png'];
+          const ext = path.extname(file.originalname);
+          if(!acceptedExtensions.includes(ext)){
+              req.file = file;
+          }
+          cb(null, acceptedExtensions.includes(ext));
+      }
+
+    });
+
 
 /* GET home page. */
 router.get('/',productController.showProduct);
 router.get('/detail/:id', productController.detail);
 router.get('/create', productController.create);
-
+router.post('/create',upload.single('image'),productController.store);
 
 module.exports = router;
