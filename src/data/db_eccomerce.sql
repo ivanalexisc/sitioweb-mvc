80b9ef13bf30aa2210ae5794663775228a3901fa
ALEo14 <aleedin14@gmail.com>
2024-08-21 23:43:18 -0300

    cambios db

--- src/data/db_eccomerce.sql
+++ src/data/db_eccomerce.sql
@@ -1,112 +1,79 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: eccomerce_real
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-08-2024 a las 04:41:48
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `categorias`
+-- Base de datos: `eccomerce_real`
 --
 
-DROP TABLE IF EXISTS `categorias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
 CREATE TABLE `categorias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `genero` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `categorias`
+-- Volcado de datos para la tabla `categorias`
 --
 
-LOCK TABLES `categorias` WRITE;
-/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` VALUES (1,'Unisex','2024-08-12 22:50:52','2024-08-12 22:50:52'),(2,'Hombre','2024-08-12 22:50:52','2024-08-12 22:50:52'),(3,'Mujer','2024-08-12 22:50:52','2024-08-12 22:50:52');
-/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `categorias` (`id`, `genero`, `created_at`, `updated_at`) VALUES
+(1, 'Unisex', '2024-08-12 22:50:52', '2024-08-12 22:50:52'),
+(2, 'Hombre', '2024-08-12 22:50:52', '2024-08-12 22:50:52'),
+(3, 'Mujer', '2024-08-12 22:50:52', '2024-08-12 22:50:52');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `colores`
+-- Estructura de tabla para la tabla `colores`
 --
 
-DROP TABLE IF EXISTS `colores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `colores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `colores`
+-- Volcado de datos para la tabla `colores`
 --
 
-LOCK TABLES `colores` WRITE;
-/*!40000 ALTER TABLE `colores` DISABLE KEYS */;
-INSERT INTO `colores` VALUES (1,'Negro','2024-08-12 22:51:51','2024-08-12 22:51:51'),(2,'Blanco','2024-08-12 22:51:51','2024-08-12 22:51:51'),(3,'Rojo','2024-08-12 22:51:51','2024-08-12 22:51:51');
-/*!40000 ALTER TABLE `colores` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `colores` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
+(1, 'Negro', '2024-08-12 22:51:51', '2024-08-12 22:51:51'),
+(2, 'Blanco', '2024-08-12 22:51:51', '2024-08-12 22:51:51'),
+(3, 'Rojo', '2024-08-12 22:51:51', '2024-08-12 22:51:51');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `producto_user`
+-- Estructura de tabla para la tabla `productos`
 --
 
-DROP TABLE IF EXISTS `producto_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `producto_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) DEFAULT NULL,
-  `id_producto` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `id_user` (`id_user`),
-  KEY `id_producto` (`id_producto`),
-  CONSTRAINT `producto_user_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id`),
-  CONSTRAINT `producto_user_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `producto_user`
---
-
-LOCK TABLES `producto_user` WRITE;
-/*!40000 ALTER TABLE `producto_user` DISABLE KEYS */;
-INSERT INTO `producto_user` VALUES (1,1,1,'2024-08-12 23:04:17','2024-08-12 23:04:17');
-/*!40000 ALTER TABLE `producto_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `id_categoria` int(11) DEFAULT NULL,
@@ -115,91 +82,191 @@ CREATE TABLE `productos` (
   `image` varchar(255) DEFAULT NULL,
   `status` enum('activo','descontinuado') DEFAULT 'activo',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  KEY `id_categoria` (`id_categoria`),
-  KEY `id_color` (`id_color`),
-  KEY `id_talle` (`id_talle`),
-  CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`),
-  CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_color`) REFERENCES `colores` (`id`),
-  CONSTRAINT `productos_ibfk_3` FOREIGN KEY (`id_talle`) REFERENCES `talles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `productos`
+-- Volcado de datos para la tabla `productos`
 --
 
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,2000.00,10,1,2,2,'oferta1.jpg','activo','2024-08-12 22:59:10','2024-08-12 22:59:10'),(2,2500.00,20,2,1,1,'oferta2.jpg','activo','2024-08-12 22:59:10','2024-08-12 22:59:10');
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `cantidad`, `id_categoria`, `id_color`, `id_talle`, `image`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'buzo nike gris', 2000.00, 10, 1, 2, 2, 'oferta1.jpg', 'activo', '2024-08-12 22:59:10', '2024-08-20 02:46:46'),
+(2, 'remera azul', 2500.00, 20, 2, 1, 1, 'oferta2.jpg', 'activo', '2024-08-12 22:59:10', '2024-08-20 02:46:46');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `talles`
+-- Estructura de tabla para la tabla `producto_user`
 --
 
-DROP TABLE IF EXISTS `talles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `producto_user` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_producto` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto_user`
+--
+
+INSERT INTO `producto_user` (`id`, `id_user`, `id_producto`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2024-08-12 23:04:17', '2024-08-12 23:04:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `talles`
+--
+
 CREATE TABLE `talles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `numero` varchar(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `talles`
+-- Volcado de datos para la tabla `talles`
 --
 
-LOCK TABLES `talles` WRITE;
-/*!40000 ALTER TABLE `talles` DISABLE KEYS */;
-INSERT INTO `talles` VALUES (1,'1','2024-08-12 22:54:53','2024-08-12 22:54:53'),(2,'2','2024-08-12 22:54:53','2024-08-12 22:54:53'),(3,'3','2024-08-12 22:54:53','2024-08-12 22:54:53');
-/*!40000 ALTER TABLE `talles` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `talles` (`id`, `numero`, `created_at`, `updated_at`) VALUES
+(1, '1', '2024-08-12 22:54:53', '2024-08-12 22:54:53'),
+(2, '2', '2024-08-12 22:54:53', '2024-08-12 22:54:53'),
+(3, '3', '2024-08-12 22:54:53', '2024-08-12 22:54:53');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estructura de tabla para la tabla `usuarios`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
   `pw_hash` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `usuarios`
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'ivan','chanenko','Espejo 3085','ivanalexischanenko@gmail.com','$2a$10$zUthv6BA6/e8TS0QMEe0A.MdGyGKmJ9l0jYQ9s83mcS7zr5Svs/xi','2024-08-12 23:03:38','2024-08-12 23:03:38');
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `direccion`, `email`, `pw_hash`, `created_at`, `updated_at`) VALUES
+(1, 'ivan', 'chanenko', 'Espejo 3085', 'ivanalexischanenko@gmail.com', '$2a$10$BLpJzkC/ho2veAlKbTsVB.slJtTXWzKq9VfA992TphZ86qZWav5hm', '2024-08-12 23:03:38', '2024-08-20 03:55:54'),
+(2, 'victor', 'chanenko', 'salta 298', 'vchanenko@gmail.com', '$2a$10$J9nlRa0nSX1iwbNI4aauK.CiCkXQ1P5lvyQCUcgpLqgLHaHgyP.km', '2024-08-20 04:22:17', '2024-08-20 04:22:17');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `colores`
+--
+ALTER TABLE `colores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_categoria` (`id_categoria`),
+  ADD KEY `id_color` (`id_color`),
+  ADD KEY `id_talle` (`id_talle`);
+
+--
+-- Indices de la tabla `producto_user`
+--
+ALTER TABLE `producto_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `talles`
+--
+ALTER TABLE `talles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `colores`
+--
+ALTER TABLE `colores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `producto_user`
+--
+ALTER TABLE `producto_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `talles`
+--
+ALTER TABLE `talles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`id_color`) REFERENCES `colores` (`id`),
+  ADD CONSTRAINT `productos_ibfk_3` FOREIGN KEY (`id_talle`) REFERENCES `talles` (`id`);
+
+--
+-- Filtros para la tabla `producto_user`
+--
+ALTER TABLE `producto_user`
+  ADD CONSTRAINT `producto_user_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `producto_user_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-08-12 20:16:36
