efc4e0c02e915433b719600b04b3398bfb979f48
ivanalexisc <iac_1999@hotmail.com>
2024-08-26 17:32:16 -0300

    fix git

--- frontend/eccomerce-front/src/main.jsx
+++ frontend/eccomerce-front/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import * as ReactDOM from "react-dom/client";
+
 import {
   createBrowserRouter,
   RouterProvider,
