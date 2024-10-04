32c8dac3d2051c6eaff2eeebadcfa085db214b82
ivanalexisc <iac_1999@hotmail.com>
2024-10-03 23:43:48 -0300

    error dialect fixed

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -22,6 +22,6 @@ module.exports = {
     password: process.env.DB_PASSWORD_PROD,
     database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST_PROD,
-    dialect: process.env.DB_DIALECT_PROD
+    dialect: 'mysql'
   }
 }
